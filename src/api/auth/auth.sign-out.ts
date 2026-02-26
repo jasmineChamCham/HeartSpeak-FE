f6b80ac79a834c42476d0fd8ef2748757b6019ba
jasmineChamCham <jasminebkdn@gmail.com>
2026-02-26 11:18:22 +0700

    feat: logout user

--- src/api/auth/auth.sign-out.ts
+++ src/api/auth/auth.sign-out.ts
@@ -1,12 +1,19 @@
 import { clearLocalStorage } from "@/common/utils";
 import apiClient from "../api.client";
+import { getDeviceId } from "./auth.refresh-token";
 
 export async function signOut() {
   try {
-    await apiClient.post("/logout");
+    const deviceId = getDeviceId();
+    await apiClient.post("/logout", { deviceId });
   } catch (error: any) {
     console.warn("API Logout failed:", error);
   } finally {
     clearLocalStorage();
+    document.cookie.split(";").forEach((c) => {
+      document.cookie = c
+        .replace(/^ +/, "")
+        .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+    });
   }
 }
